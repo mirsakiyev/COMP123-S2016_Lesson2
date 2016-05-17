1a12c105004c08cc7c35c3e48ffc3e9deb3788e7
mirsakiyev <mirsakiyev@yahoo.com>
2016-05-17 12:55:17 -0400

    Added a reference to driver class from UnitTest

--- COMP123-S2016-Lesson2/Program.cs
+++ COMP123-S2016-Lesson2/Program.cs
@@ -4,10 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+* Author: Aslan Mirsakiyev
+* Date: May 17, 2016
+* Date MOdified : May 17, 2016
+* Description : Advanced Methods Demo for Lesson 2
+* Version 0.0.2 - Added a reference to driver class from UnitTest
+*/
+
+
 namespace COMP123_S2016_Lesson2
 {
+    /**
+    * Main driver class for Lesson 2
+    *
+    * @class Program
+    */
     class Program
     {
+        /**
+        * Main method for our driver class
+        *
+        * @constructor
+        * @param {string[]} args
+        */
         static void Main(string[] args)
         {
         }
--- UnitTestForLesson2/UnitTest1.cs
+++ UnitTestForLesson2/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using COMP123_S2016_Lesson2;
 
 namespace UnitTestForLesson2
 {
@@ -9,6 +10,11 @@ namespace UnitTestForLesson2
         [TestMethod]
         public void TestMethod1()
         {
+            // Arrange
+
+            // Act
+
+            // Assert
         }
     }
 }
